d3315ccb04ebad91c9afc0d90f9d2392bd2c5ea6
David Betancourt <dmarquez.develop@gmail.com>
2025-07-20 19:27:59 +0000

    added todo list but needs card shadow effect

--- src/js/components/Todolist.jsx
+++ src/js/components/Todolist.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 
 const Todolist = () => {
-    const [inputValue , setInputValue] = useState('No hay tareas, añadir tareas')
+    const [inputValue, setInputValue] = useState('')
     const [task, setTasks] = useState([]);
 
     function handleInputChange(e) {
@@ -11,28 +11,61 @@ const Todolist = () => {
     }
 
     function addTaskToList() {
-        if (inputValue.trim()) { 
+        if (inputValue.trim()) {
             setTasks(prevTasks => [...prevTasks, inputValue]);
             setInputValue('');
+        }
     }
-}
+
+    function deleteTask(index){
+        const newList = task.filter((_, i) => i !== index);
+        setTasks(newList);
+    }
 
     return (
-        <div className="text-center">
-            <h1>Todo</h1>
-            <div>
-                <label htmlFor="task">Task: </label>
-                <input 
-                type="text" 
-                value={inputValue} 
-                id="task" 
-                onChange={handleInputChange} 
-                onKeyPress={(e) => {if (e.key === 'Enter') {
-                            addTaskToList();
-                        }}}/>
-                <button onClick={addTaskToList}>Save</button>
+        <div className="container">
+            <div className="row">
+                <div className="title">
+                    <h1>Todos</h1>
+                </div>
+                <div className="tasks col-12 p-0">
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={inputValue}
+                        placeholder="What needs to be done?"
+                        onChange={handleInputChange}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                addTaskToList();
+                            }
+                        }} />
+                </div>
+                <ul className="list-group col-12 p-0">
+					{task.map((task, index) => (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between">
+							<span>{index + 1}. {task}</span>
+							<button
+								onClick={() => deleteTask(index)}
+								style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}
+							>
+								<i className="far fa-trash-alt"></i>
+							</button>
+						</li>
+					))}
+				</ul>
+                {task.length > 0 ? (
+					<div>
+						{task.length} item{task.length === 1 ? '' : 's'} left
+					</div>
+				) : (
+					<div>
+						No hay tareas, añadir tareas
+					</div>
+				)}
             </div>
-            {task.map((tarea,index) => <ul key={index}>Task {index + 1 }: {tarea}</ul>)}  
         </div>
     )
 }
